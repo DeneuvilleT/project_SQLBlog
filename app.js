d96ede34d5fcdaa12912a0779621c5f4f84a6910
DeneuvilleT <deneuville.thomas@gmail.com>
2022-04-22 20:31:01 +0200

    22/04/2022

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 import { fileURLToPath } from "url";
 import path from "path";
 import express from 'express';
@@ -9,10 +8,12 @@ const PORT = 9000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
-
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 const pool = mysql.createPool({
@@ -21,30 +22,109 @@ const pool = mysql.createPool({
    user: "root",
    password: "",
 });
-
 console.log(`Connection à ${pool.config.connectionConfig.database}`);
 
+
+
+
+// ************************************************************************************************ Home
 app.get('/', (req, res) => {
-   pool.query('SELECT NickName, Contents, CreationTimestamp FROM comment', function (error, results, fields) {
+   pool.query(`
+   SELECT post.Id,Title,Contents,CreationTimestamp,FirstName, LastName 
+   FROM post JOIN author ON post.Author_Id = author.Id`, (error, results) => {
       if (error) {
          throw Error;
       } else {
-         console.log(results);
-         res.render("layout", { template: "home", data: results})
-      }
+         res.render("layout", { template: "home", data: results });
+      };
    });
 });
 
 
-app.get('/commentPage', (req, res) => {
-   pool.query('SELECT * FROM comment', function (error, results, fields) {
+// ************************************************************************************************ Ajout d'un article
+app.get('/newPost', (req, res) => {
+   pool.query("SELECT * FROM category JOIN author GROUP BY category.Id", (error, results) => {
       if (error) {
          throw Error;
       } else {
-         res.render("layout", { template: "commentPage", data: results})
+         res.render("layout", { template: "newPost", data: results })
+         console.log(results)
       }
    });
+   app.post('/newPost', (req, res) => {
+      pool.query("INSERT INTO post (Id, Title, Contents, CreationTimestamp, Author_Id, Category_Id) VALUES(NULL, ?, ?, NOW(), 1, 1)",
+         [req.body.title, req.body.contents, req.body.user], (error, results) => {
+            if (error) {
+               throw Error;
+            } else {
+               res.redirect('/newPost');
+               console.log(results);
+            }
+         });
+   });
 });
+
+
+
+
+// ************************************************************************************************ Admin
+app.get('/admin', (req, res) => {
+   pool.query(`
+   SELECT Name,post.Id,Title,Contents,CreationTimestamp,FirstName, LastName, Category_Id 
+   FROM post JOIN author ON post.Author_Id = author.Id
+   INNER JOIN category ON category.Id = post.Category_Id`,
+      (error, results) => {
+         res.render("layout", { template: "admin", data: results })
+      });
+});
+
+app.get('/delete/:id', (req, res) => {
+   let id = req.params.id;
+   pool.query('DELETE FROM post WHERE post.Id = ?', [id], (error, results) => {
+      res.redirect('/admin');
+   });
+});
+
+
+
+// app.get('/edit/:id', (req, res) => {
+//    let id = req.params.id;
+//    pool.query('INSERT FROM post  ');
+// });
+
+
+
+// ************************************************************************************************ Details
+app.get('/details/:id', (req, res) => {
+   let id = req.params.id;
+   pool.query('SELECT * FROM post WHERE Id = ?', [id], (error, commentOne) => {
+
+      pool.query(`
+      SELECT * FROM post 
+      JOIN comment JOIN author 
+      ON post.Id = comment.Post_Id 
+      WHERE author.Id = 1 AND Post_Id = ?`, [id], (error, results) => {
+         res.render("layout", { template: "details", comment: commentOne, data: results });
+      });
+   });
+
+   app.post('/details', (req, res) => {
+      pool.query(`
+      INSERT INTO comment (NickName, Contents,CreationTimestamp, Post_Id)
+      VALUES (?,?,CURDATE(),?)`, [req.body.nickname, req.body.contents, id], (err, result) => {
+         if (err) {
+            console.log(err);
+         };
+         res.redirect(`/details/${id}`);
+      });
+   });
+});
+
+
+
+
+
+
 
 
 
